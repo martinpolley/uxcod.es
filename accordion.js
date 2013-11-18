27668f608afbf833badba53f8c84c774eabd6d5e
martinpolley <martin.polley@gmail.com>
2013-11-18 15:10:14 +0200

    Create accordion.js

---
+++ accordion.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+/* Converts the h3s and divs into an accordion.
+   active: false collapses all the panels initially.
+   collapsible: true is necessary for active: false to work.
+   heightStyle: "content" makes each panel as tall as its contents.
+*/
+  $('#settings_accordion').accordion(
+    { 
+      collapsible: true,
+      active: false,
+      heightStyle: "content"
+    }
+  );
+  
+  /* Attaches a handler function to the notifications checkbox that toggles the visibility of the notification sub-options. */
+  $('input#cb_enable_notify').change(function(){
+    $('div.sub_options').toggle();
+  });
+  
+});
